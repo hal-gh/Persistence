537d53eab3de332ab5eeee2bf39211f2351c4f95
hal <github-dev@netaxiom.net>
2010-06-21 13:51:54 +0100

    Part of work done to support android

--- Resources/database.js
+++ Resources/database.js
@@ -1,47 +1,65 @@
 var win = Titanium.UI.currentWindow;
+win.backgroundColor = '#fff';
 win.layout = 'vertical';
 var currentNote = '';
 var db = Titanium.Database.open('todos');
+
+var CONTROL_HEIGHT = 34; 
+	
 db.execute('CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, todo TEXT)');
+
+// Workaround for problems using percentage widths and heights up to and including Ti 1.2.1
+function percentageToPixels(type,percent,margins,length) {
+	if(type != 'width' || type != 'height' ) { alert('Error: type argument (width/height) not set'); };
+		
+	if (margins == null) {
+		margins = 0;
+	}
+	if (length == null) {
+		if(type == 'height') {
+			length = Ti.Platform.displayCaps.platformHeight-(margins*2);
+		} else {
+			length = Ti.Platform.displayCaps.platformWidth-(margins*2);
+		}
+	}
+
+	return new Number((length * (percent/100))).toFixed(0);
+}
 
 //create data entry view
 var entryView = Ti.UI.createView({
-  backgroundColor:'#0060AA',
-  width:'auto',
-  height:100,
-  top:5
+  height:'auto',
+  width:percentageToPixels('width',100)
 });
 
 var controlsView = Ti.UI.createView({
-  width:'auto',
-  height:50
+	backgroundColor:'#6C6C6C',
+	height:'auto',
+	width:percentageToPixels('width',100)
 });
 
-var controlsViewSub = Ti.UI.createView({
-	  width:'auto',
-	  height:50
-	});
-
-var b1 = Titanium.UI.createButton({
-	title:'Save',
-	width:'25%',
-	height:35,
-	right:0,
-	enabled:false
+var controlsViewEdit = Ti.UI.createView({
+	backgroundColor:'#6C6C6C',
+	height:'auto',
+	width:percentageToPixels('width',100),
+	top:1 // just for testing
 });
-controlsView.add(b1);
 
 var tf1 = Titanium.UI.createTextField({
-	width:'25%',
-	height:35,
-	left:0,
-	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
 	autocorrect:false,
-	hintText:'Enter a note...'
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	font:{fontSize:14},
+	height:CONTROL_HEIGHT,
+	hintText:'Enter a note...',
+	left:percentageToPixels('width',1,1,entryView.width),
+	width:percentageToPixels('width',70,1,entryView.width),
+	verticalAlign:'middle' //testing
 });
+
 tf1.addEventListener('return', function() {
 	tf1.blur();
 });
+
 tf1.addEventListener("change", function(e) {
   currentNote = e.value;
   if (currentNote == '') {
@@ -53,8 +71,17 @@ tf1.addEventListener("change", function(e) {
 });
 controlsView.add(tf1);
 
+var b1 = Titanium.UI.createButton({
+	enabled:false,
+	height:CONTROL_HEIGHT,
+	left:percentageToPixels('width',74,1,entryView.width),
+	title:'Save',
+	width:percentageToPixels('width',25,1,entryView.width)
+});
+controlsView.add(b1);
+
 entryView.add(controlsView);
-win.add(entryView);
+
 
 //This is the array we'll use to back the table view
 var data = [];
@@ -75,7 +102,8 @@ var tableview = Titanium.UI.createTableView({
 	backgroundColor:'#EDECEB',
 	data:data,
 	editable:true,
-	top:200
+//	height:Ti.Platform.displayCaps.platformHeight-200
+	bottom: percentageToPixels('height',15,0)
 });
 
 // create table view event listener
@@ -91,11 +119,9 @@ tableview.addEventListener('delete',function(e) {
   db.execute("DELETE FROM todos WHERE id = ?", e.rowData.id);
 });
 
-// add table view to the window
-Titanium.UI.currentWindow.add(tableview);
-
 //Add event listener for save button
 b1.addEventListener("click", function(e) {
+	controlsViewEdit.hide;
   if (b1.enabled) {
     db.execute('INSERT INTO todos (todo) VALUES(?)',currentNote);
     var last = db.execute("SELECT * FROM todos ORDER BY id DESC LIMIT 1");
@@ -115,27 +141,43 @@ b1.addEventListener("click", function(e) {
 //  create edit/cancel buttons for nav bar
 //
 var edit = Titanium.UI.createButton({
-	title:'Edit'
+	title:'Edit',
+	height: CONTROL_HEIGHT,
+	left:percentageToPixels('width',15,0,entryView.width),
+	width:percentageToPixels('width',25,0,entryView.width)
 });
+
 var cancel = Titanium.UI.createButton({
 	title:'Cancel',
-	style:Titanium.UI.iPhone.SystemButtonStyle.DONE
+	height: CONTROL_HEIGHT,
+	right:percentageToPixels('width',15,0,entryView.width),
+	width:percentageToPixels('width',25,0,entryView.width)
+//	style:Titanium.UI.iPhone.SystemButtonStyle.DONE
 });
 
-if (Ti.Platform.osname == 'iphone') {
-	edit.addEventListener('click', function() {
+
+edit.addEventListener('click', function() {
+	if (Ti.Platform.osname == 'iphone') {
 		win.setRightNavButton(cancel);
-		tableview.editing = true;
-	});
-	cancel.addEventListener('click', function() {
+	} else {
+//		controlsViewSub.add(cancel);
+	}
+	tableview.editing = true;
+});
+cancel.addEventListener('click', function() {
+	if (Ti.Platform.osname == 'iphone') {
 		win.setRightNavButton(edit);
-		tableview.editing = false;
-	});
+	} else {
+//		controlsViewSub.add(edit);
+	}
+	tableview.editing = false;
+});
 
-	win.setRightNavButton(edit);	
-}
+controlsViewEdit.add(edit);
+controlsViewEdit.add(cancel);
 
-//if (Ti.Platform.osname == 'android') {
-//	controlsViewSub.add(edit);
-//	controlsViewSub.add(cancel);
-//}
+win.add(entryView);
+win.add(controlsViewEdit);
+
+//add table view to the window
+win.add(tableview);
